b9febe814155bb6c0ce2a09f88ed5d237e0356d5
sannya07 <sanyakansal07@gmail.com>
2025-08-14 19:21:16 +0530

    created team routes

--- Backend/routes/teamRoutes.js
+++ Backend/routes/teamRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createTeam, getMyTeams } from '../controller/teamController.js';
+import { createTeam, getMyTeams, joinTeam } from '../controller/teamController.js';
 import { isAuthenticated,authorizeRoles } from '../middleware/authMiddleware.js';
 
 const router=express.Router();
 
 router.post('/',isAuthenticated,authorizeRoles('Admin','Project Manager'),createTeam);
 router.get('/',isAuthenticated,getMyTeams)
+router.post('/:id/join',isAuthenticated,joinTeam)
+
 export default router;
