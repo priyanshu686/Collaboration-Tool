9705d311f2ae4f2166365471fa08d8debcf41cc9
Yashasvi-Nag <nag.yashasvi@gmail.com>
2025-08-18 15:14:09 +0530

    listTask for project_manager, listTask for projectMember, delete Task

--- Backend/controller/taskController.js
+++ Backend/controller/taskController.js
@@ -59,3 +59,37 @@ export const createTask = async(req,res)=>{
         })
     }
 }
+
+//List all tasks in this project (projectManager's POV)
+export const listTasksManager= async(req,res)=>{
+    try{
+        const {projectId}= req.params;
+        const tasks= await Task.find({project:projectId}).populate("assignees","name email");
+        res.json(tasks);
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
+};
+
+//List only assigned tasks for logged-in team member
+export const listTasksMember= async (req,res)=>{
+    try{
+        const {projectId}=req.params;
+        const userId= req.user._id;
+        const tasks= await Task.find({project: projectId, assignees: userId});
+        res.json(tasks);
+    }catch (err){
+        res.status(400).json({error:err.message});
+    }
+};
+
+//Delete a task
+export const deleteTask= async (req,res)=>{
+    try{
+        const {taskId}=req.params;
+        await Task.findByIdAndDelete(taskId);
+        res.json({message:"Task deleted."});
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
+};
