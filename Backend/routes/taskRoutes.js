bb284fa30255505692fef48ea937198f5d5ccd87
Yashasvi-Nag <nag.yashasvi@gmail.com>
2025-08-18 15:35:02 +0530

    route for list task, delete task

--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -4,5 +4,8 @@ import { isAuthenticated , authorizeRoles} from '../middleware/authMiddleware.js
 import { createTask } from '../controller/taskController.js';
 
 router.post('/create/:projectId/:TeamId/:AssignedId',isAuthenticated,authorizeRoles('Admin','Project Manager'),createTask)
+router.get("/tasks/manager/:projectId",isAuthenticated,authorizeRoles("project_manager"),listTasksManager);
+router.get("/tasks/member/:projectId",isAuthenticated,listTasksMember);
+router.delete("/tasks/:taskId",isAuthenticated,authorizeRoles("project_manager"), deleteTask);
 
 export default router
